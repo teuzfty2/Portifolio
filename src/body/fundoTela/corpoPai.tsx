cf86bdfa62bff0b95755887715b04b06e6d31a7e
matheuss <matheussfilipej@gmail.com>
2025-07-21 21:03:38 -0300

    as

--- src/body/fundoTela/corpoPai.tsx
+++ src/body/fundoTela/corpoPai.tsx
@@ -1,4 +1,3 @@
-import { div } from "framer-motion/client";
 import LuzMouse from "./planoFundo";
 
 export default function CorpoPai() {
@@ -6,12 +5,14 @@ export default function CorpoPai() {
   return (
     <div className="flex min-h-screen w-full bg-gradient-to-b from-[#0f1729] to-[#0f1729]  text-white">
       <LuzMouse />
+
       {/* Titulo nome esquerda*/}
-      <div className="flex rounded-lg w-full justify-center items-center  h-full  bg-gray-700 border border-none">
-        <h1 className="text-4xl font-bold tracking-tight text-slate-200">
+      <div className="flex rounded-lg w-full justify-center  backdrop-blur-2xl bg-gray-700/40 border border-none ">
+        <span className="flex  text-4xl font-bold tracking-tight text-slate-100 ">
           Matheus Filipe
-        </h1>
+        </span>
       </div>
+
       {/* Titulo nome direita*/}
       <div className="flex justify-center w-full  bg-[#0f1729] border border-none">
         <h1>Explicações</h1>
